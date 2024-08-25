b911109bbb71008416a4297d408050fc2d395beb
Nandita Raj Kumar <nanditark@Nanditas-MacBook-Pro.local>
2024-08-24 21:52:47 -0700

    promise resolvers issue fix attempt 1

--- src/app/spring/page.tsx
+++ src/app/spring/page.tsx
@@ -7,6 +7,21 @@ import { pdfjs } from 'react-pdf';
 import PdfViewer from '../components/PdfViewer';
 
 const Fall = () => {
+
+  // @ts-expect-error This does not exist outside of polyfill which this is doing
+if (typeof Promise.withResolvers === 'undefined') {
+  if (window)
+      // @ts-expect-error This does not exist outside of polyfill which this is doing
+      window.Promise.withResolvers = function () {
+          let resolve, reject;
+          const promise = new Promise((res, rej) => {
+              resolve = res;
+              reject = rej;
+          });
+          return { promise, resolve, reject };
+      };
+}
+
     
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
     
