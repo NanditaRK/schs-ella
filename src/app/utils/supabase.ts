7ceba26a972da8e77043c6cadf0184937d0b91ae
Nandita Raj Kumar <nanditarajkumar@gmail.com>
2025-05-29 10:10:16 -0700

    supabase keys

--- src/app/utils/supabase.ts
+++ src/app/utils/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 if (!supabaseUrl) {
   throw new Error('Supabase URL is not set in environment variables.');
